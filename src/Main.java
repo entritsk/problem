72accf6b7cf94eced02f3034883e30557017444c
entritsk <entritsokou@gmail.com>
2024-10-08 11:16:01 +0300

    test re malaka

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner ; //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+Scanner myObj = new Scanner(System.in) ;
+System.out.println("kyrios malakas: " ) ;
+String malakas = myObj.nextLine() ;
+System.out.println("o kyrios malakas einai o :" + malakas ) ;
+
+    }
+}
